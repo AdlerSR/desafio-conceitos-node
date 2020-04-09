58eb58d2818bdc6f1a403cf04a05da1c89fed262
Adler Santos Rodrigues <adlerdet@outlook.com>
2020-04-09 16:30:19 -0300

    Complete CRUD with node

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,83 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", (req, res) => {
+  return res.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body;
+
+  const repository = {id: uuid(), title, url, techs, likes: 0}
+
+  repositories.push(repository);
+
+  return res.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, url, techs } = req.body;
+
+  const repositoriesIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoriesIndex < 0){
+    return res.status(400).json({error: "Repository not found"});
+  }
+  
+  const { likes } = repositories[repositoriesIndex];
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  }
+
+  repositories[repositoriesIndex] = repository;
+
+  return res.json(repository);
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params;
+
+  const repositoriesIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoriesIndex < 0){
+    return res.status(400).json({error: "Repository not found"});
+  }
+
+  repositories.splice(repositoriesIndex, 1)
+  
+  return res.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", (req, res) => {
+  const { id } = req.params;
+
+  const repositoriesIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoriesIndex < 0){
+    return res.status(400).json({error: "Repository not found"});
+  }
+
+  const { title, url, techs, likes } = repositories[repositoriesIndex];
+
+  const like = likes+1
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: like
+  }
+
+  repositories[repositoriesIndex] = repository;
+
+  return res.json(repository);
 });
 
 module.exports = app;
